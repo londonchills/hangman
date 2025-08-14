21ad6c142bde4d14c4acf4b56468523cc24aa748
Tyreece Coleman-Paul <tycolemanpaul@gmail.com>
2025-08-14 03:48:49 +0100

    shows current word???

--- hangman.py
+++ hangman.py
@@ -18,6 +18,9 @@ def hangman():
     while len(word_letters) > 0:
         print('You have used these letters: ', ' '.join(used_letters))
 
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
