a6336df56784597cbc7362183d1a19dd39bb04c6
Tyreece Coleman-Paul <tycolemanpaul@gmail.com>
2025-08-14 03:14:07 +0100

    import string

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 import random
 from words import words
+import string
 
 def get_valid_word(words):
     word = random.choice(words)
@@ -11,4 +12,4 @@ def get_valid_word(words):
 def hangman()
     word = get_valid_word(words)
     word_letters = set(word)
-    
+
