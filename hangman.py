202f71ee81c9f374198a1015bc559bc126071600
Tyreece Coleman-Paul <tycolemanpaul@gmail.com>
2025-08-14 03:42:34 +0100

    while loop, while number of word letters left is greater than zero, keep asking for input

--- hangman.py
+++ hangman.py
@@ -15,16 +15,17 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
    
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
-    elif user_letter in used_letters:
-        print('Letter already been used. Try again')
-    
-    else:
-        print('Invalid character. Try again')
+        elif user_letter in used_letters:
+            print('Letter already been used. Try again')
+        
+        else:
+            print('Invalid character. Try again')
 
 hangman()
