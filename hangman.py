f7da97ce74162e47e3393421ffc65bf2703da0d3
Tyreece Coleman-Paul <tycolemanpaul@gmail.com>
2025-08-14 03:56:11 +0100

    adding to while loop, keep iterating while length of word_letters is greater than zero and while lives is greater than zero

--- hangman.py
+++ hangman.py
@@ -17,8 +17,8 @@ def hangman():
 
     lives = 6
    
-    while len(word_letters) > 0:
-        print('You have used these letters: ', ' '.join(used_letters))
+    while len(word_letters) > 0 and lives > 0:
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print('Current word: ', ' '.join(word_list))
